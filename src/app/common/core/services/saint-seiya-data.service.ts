d1614a57f055ddb485631c23b82e43ba5fcc2957
chesteric31 <chesteric31@gmail.com>
2019-03-19 21:19:54 +0100

    Fix env api url

--- src/app/common/core/services/saint-seiya-data.service.ts
+++ src/app/common/core/services/saint-seiya-data.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 import {Armor} from '../../interfaces/saint-seiya';
+import {environment} from '../../../../environments/environment';
 
 @Injectable()
 export class SaintSeiyaDataService {
@@ -12,9 +13,9 @@ export class SaintSeiyaDataService {
   constructor(private http: HttpClient) {
     const params = new HttpParams().set('scaleHeight', '185').set('scaleWidth', '185');
 
-    const url = 'https://ksams.herokuapp.com/api/v2/armors/';
-    const devUrl ='http://localhost:8081/api/v2/armors/';
-    this.allArmors = this.http.get<Armor[]>(devUrl, {params});
+    const url = environment.apiUrl;//'https://ksams.herokuapp.com/api/v2/armors/';
+    //const devUrl ='http://localhost:8081/api/v2/armors/';
+    this.allArmors = this.http.get<Armor[]>(url, {params});
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://ksams.herokuapp.com/api/v2/armors/'
 };
