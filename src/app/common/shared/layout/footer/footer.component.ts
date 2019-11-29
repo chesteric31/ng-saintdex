41e2601de57c545cc07b5fa3bb74471b3ccc46e1
chesteric31 <eric.binard.182@gmail.com>
2019-11-29 22:37:17 +0100

    Fix app version modifier

--- src/app/common/shared/layout/footer/footer.component.ts
+++ src/app/common/shared/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import {environment} from '../../../../../environments/environment';
 })
 export class FooterComponent implements OnInit {
 
-  private appVersion: string;
+  appVersion: string;
 
   constructor() { }
 
